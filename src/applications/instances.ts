889112fcb8d882e833fcf8735a9e0f0c598b84f1
Indra Zulfi Mushoddaq <me@indrazm.com>
2024-12-02 21:49:10 +0700

    fix

--- src/applications/instances.ts
+++ src/applications/instances.ts
@@ -5,4 +5,4 @@ import { LoggerProd } from "../infrastructure/logger/loggerProd";
 
 const logger = process.env.NODE_ENV === "production" ? new LoggerProd() : new LoggerDev();
 const noteRepository = new NoteRepositoryProd(logger);
-export const noteServices = new NoteServices(noteRepository);
+export const noteServices = new NoteServices(noteRepository, logger);
--- src/applications/services/noteServices.ts
+++ src/applications/services/noteServices.ts
@@ -1,15 +1,19 @@
 // Business Logic Layer - Services
 
 import { NoteRepositoryLocal } from "../../infrastructure/db/noteRepo.local";
+import { ILogger } from "../../infrastructure/entity/interface";
 
 export class NoteServices {
   private noteRepo: NoteRepositoryLocal;
+  private logger: ILogger;
 
-  constructor(noteRepo: NoteRepositoryLocal) {
+  constructor(noteRepo: NoteRepositoryLocal, logger: ILogger) {
     this.noteRepo = noteRepo;
+    this.logger = logger;
   }
 
   async getAll() {
+    this.logger.info("Get all from noteServices");
     // redis => memory cache
     return [];
   }
